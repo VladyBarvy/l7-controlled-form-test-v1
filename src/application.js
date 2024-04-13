01132a139441f2f658038a2bbbc1b3b04c3386a5
VladyBarvy <vladbarskoff@yandex.ru>
2024-04-13 23:08:55 +0300

    step 1 2

--- src/application.js
+++ src/application.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
-import onChange from 'on-change';
-
+// import onChange from 'on-change';
+/*
 const validateName = (name) => (name.trim().length ? [] : ['name cannot be empty']);
 const validateEmail = (email) => (/\w+@\w+/.test(email) ? [] : ['invalid email']);
-const validateField = (fieldname, data) => (fieldname === 'name' ? validateName(data) : validateEmail(data));
-
+const validateField = (fieldname, data) => {
+  fieldname === 'name' ? validateName(data) : validateEmail(data)
+};
+*/
 function addForm() {
   const formData = document.createElement('div');
   const formContent = document.getElementsByClassName('form-container')[0];
   formData.innerHTML = `
     <form id="registrationForm">
-        <div class="form-group">
-            <label for="inputName">Name</label>
-            <input type="text" class="form-control" id="inputName" placeholder="Введите ваше имя" name="name" required>
-        </div>
-        <div class="form-group">
-            <label for="inputEmail">Email</label>
-            <input type="text" class="form-control" id="inputEmail" placeholder="Введите email" name="email" required>
-        </div>
-        <input type="submit" value="Submit" class="btn btn-primary">
+      <div class="form-group">
+        <label for="inputName">Name</label>
+        <input type="text" class="form-control" id="inputName" placeholder="Введите ваше имя" name="name" required>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail">Email</label>
+        <input type="text" class="form-control" id="inputEmail" placeholder="Введите email" name="email" required>
+      </div>
+      <input type="submit" value="Submit" class="btn btn-primary">
     </form>
   `;
   formContent.appendChild(formData);
-};
-
-
+}
 
 export default function app() {
   // step 1
@@ -37,13 +37,13 @@ export default function app() {
   const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    axios.post('/users',{})
-    .then((response) => {
-      document.body.innerHTML = `<p>${response.data.message}</p>`;
-  })
-  .catch(function (error) {
-      console.log(error);
-    });
+    axios.post('/users', {})
+      .then((response) => {
+        document.body.innerHTML = `<p>${response.data.message}</p>`;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
   // step 2
 
@@ -63,19 +63,16 @@ export default function app() {
   };
 
   const watchedState = onChange(state, (path) => {
-    
     const selector = path.split('.')[1];
     const input = document.querySelector(`name=${selector}`);
     console.log(input);
-    
-    //  if (validateField(selector, state.values[selector]).length === 0) {
+    //   if (validateField(selector, state.values[selector]).length === 0) {
     //   input.classList.remove('is-valid');
     //   input.classList.add('is-invalid');
     // } else {
     //   input.classList.remove('is-invalid');
     //   input.classList.add('is-valid');
-    // } 
-    
+    // }
   });
 
   form.addEventListener('input', (e) => {
@@ -87,8 +84,4 @@ export default function app() {
   });
 */
   // step 3
-
-
 }
-
-
